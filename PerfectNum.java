e68dd207eb24742f039da477345030909acef139
Budhewar <anuradhabudhewar@gmail.com>
2022-12-03 22:34:50 +0530

    Logical programming

---
+++ PerfectNum.java
@@ -0,0 +1,23 @@
+
+package com.bridgelabz.logical;
+
+import java.util.Scanner;
+
+public class PerfectNum {
+
+	public static void main(String[] args) {
+		int divisorsSum = 1;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter any number: ");
+		int num = scanner.nextInt();
+		
+		for (int i = 2; i <= num / 2; i++)
+			if (num % i == 0)
+				divisorsSum += i;
+		
+		if (num == divisorsSum)
+			System.out.println(num + " is a perfect number!");
+		else
+			System.out.println(num + " is not perfect number!");
+	}
+}
---
+++ ReversrNum.java
@@ -0,0 +1,20 @@
+
+package com.bridgelabz.logical;
+
+import java.util.Scanner;
+
+class ReverseNum {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter any number of digits: ");
+		int num = scan.nextInt();
+
+		int digit, rev = 0;
+		while (num != 0) {
+			digit = num % 10;
+			rev = rev * 10 + digit;
+			num = num / 10;
+		}
+		System.out.println("Reverse number = " + rev);
+	}
+}
